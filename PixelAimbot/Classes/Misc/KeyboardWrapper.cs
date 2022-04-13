caead24b29ae0d342fb08361eb21dc929dc050bd
inZaneX <54487336+inZaneX@users.noreply.github.com>
2022-04-14 00:22:53 +0200

    AlternateHoldKey hinzugefügt

--- PixelAimbot/Classes/Misc/KeyboardWrapper.cs
+++ PixelAimbot/Classes/Misc/KeyboardWrapper.cs
@@ -66,6 +66,18 @@ namespace PixelAimbot.Classes.Misc
                 totalDuration += PauseBetweenStrokes;
             }
         }
+
+        public static void AlternateHoldKey(byte key, int duration)
+        {
+            int totalDuration = 0;
+            while (totalDuration < duration)
+            {
+                keybd_event(key, 0, KEY_DOWN_EVENT, 0);
+                System.Threading.Thread.Sleep(PauseBetweenStrokes);
+                totalDuration += PauseBetweenStrokes;
+            }
+            keybd_event(key, 0, KEY_UP_EVENT, 0);
+        }
         /// <summary>
         /// Will press a key
         /// </summary>
