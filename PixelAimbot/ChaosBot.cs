28bb0ca9e6b42d48552204104f0a932f0694504e
inZaneX <54487336+inZaneX@users.noreply.github.com>
2022-04-14 00:29:03 +0200

    STARTKLICK wieder geän dert

--- PixelAimbot/ChaosBot.cs
+++ PixelAimbot/ChaosBot.cs
@@ -304,7 +304,7 @@ namespace PixelAimbot
                     _stop = true;
                     cts = new CancellationTokenSource();
                     var token = cts.Token;
-                    var t1 = Task.Run(() => RESTART(token));
+                    var t1 = Task.Run(() => STARTKLICK(token));
 
                     if (chBoxAutoRepair.Checked == true && _start == true)
                     {
