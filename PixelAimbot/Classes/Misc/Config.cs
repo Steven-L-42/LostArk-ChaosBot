32d0342389c27058d13179a1cb1e99c10d7f8f53
inZaneX <54487336+inZaneX@users.noreply.github.com>
2022-04-15 13:00:18 +0200

    Login Fix, Patchnotes change

--- PixelAimbot/Classes/Misc/Config.cs
+++ PixelAimbot/Classes/Misc/Config.cs
@@ -15,7 +15,7 @@ namespace PixelAimbot.Classes.Misc
     
     public class Config
     {
-        public static string version { get; set; } = "2.0.0r";
+        public static string version { get; set; } = "1.9.9r";
         public string username { get; set; } = "";
         public string password { get; set; } = "";
 
