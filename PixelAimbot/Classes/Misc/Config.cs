df35162134d373d21ac4e17f805f47db81ab86a9
ShiiikK <102829233+ShiiikK@users.noreply.github.com>
2022-04-15 11:42:00 +0200

    restart new

--- PixelAimbot/Classes/Misc/Config.cs
+++ PixelAimbot/Classes/Misc/Config.cs
@@ -15,7 +15,7 @@ namespace PixelAimbot.Classes.Misc
     
     public class Config
     {
-        public static string version { get; set; } = "1.9.7r";
+        public static string version { get; set; } = "1.9.8r";
         public string username { get; set; } = "";
         public string password { get; set; } = "";
 
