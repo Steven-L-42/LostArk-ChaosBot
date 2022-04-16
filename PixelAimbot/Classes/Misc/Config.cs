2a779652e97acce39c38251692c502e39fcc7527
ShiiikK <102829233+ShiiikK@users.noreply.github.com>
2022-04-16 22:34:36 +0200

    update 2.1.7r fix api

--- PixelAimbot/Classes/Misc/Config.cs
+++ PixelAimbot/Classes/Misc/Config.cs
@@ -15,7 +15,7 @@ namespace PixelAimbot.Classes.Misc
     
     public class Config
     {
-        public static string version { get; set; } = "2.1.6r";
+        public static string version { get; set; } = "2.1.7r";
         public string username { get; set; } = "";
         public string password { get; set; } = "";
 
