0d70e30a0ef91f11c193119a9c8fa2054a9458e1
inZaneX <54487336+inZaneX@users.noreply.github.com>
2022-04-16 13:19:49 +0200

    Anpassungen Debugger

--- PixelAimbot/Debugging.Designer.cs
+++ PixelAimbot/Debugging.Designer.cs
@@ -31,13 +31,20 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.textBoxWidth = new System.Windows.Forms.TextBox();
+            this.textBoxHeight = new System.Windows.Forms.TextBox();
+            this.labelX = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(631, 378);
@@ -46,29 +53,105 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(660, 83);
+            this.button1.Location = new System.Drawing.Point(660, 305);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(169, 60);
+            this.button1.Size = new System.Drawing.Size(169, 27);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Screenshot";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(660, 169);
+            this.button2.Location = new System.Drawing.Point(660, 338);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(169, 60);
+            this.button2.Size = new System.Drawing.Size(169, 27);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Multiple";
+            this.button2.Text = "Live";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(754, 21);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(100, 20);
+            this.textBoxX.TabIndex = 3;
+            this.textBoxX.TextChanged += new System.EventHandler(this.textBoxX_TextChanged);
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Location = new System.Drawing.Point(754, 47);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(100, 20);
+            this.textBoxY.TabIndex = 4;
+            this.textBoxY.TextChanged += new System.EventHandler(this.textBoxY_TextChanged);
+            // 
+            // textBoxWidth
+            // 
+            this.textBoxWidth.Location = new System.Drawing.Point(754, 73);
+            this.textBoxWidth.Name = "textBoxWidth";
+            this.textBoxWidth.Size = new System.Drawing.Size(100, 20);
+            this.textBoxWidth.TabIndex = 5;
+            this.textBoxWidth.TextChanged += new System.EventHandler(this.textBoxWidth_TextChanged);
+            // 
+            // textBoxHeight
+            // 
+            this.textBoxHeight.Location = new System.Drawing.Point(754, 99);
+            this.textBoxHeight.Name = "textBoxHeight";
+            this.textBoxHeight.Size = new System.Drawing.Size(100, 20);
+            this.textBoxHeight.TabIndex = 6;
+            this.textBoxHeight.TextChanged += new System.EventHandler(this.textBoxHeight_TextChanged);
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Location = new System.Drawing.Point(660, 24);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(79, 13);
+            this.labelX.TabIndex = 7;
+            this.labelX.Text = "X Start Position";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(660, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Y Start Position";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(660, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Width";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(660, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Height";
             // 
             // Debugging
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(866, 377);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelX);
+            this.Controls.Add(this.textBoxHeight);
+            this.Controls.Add(this.textBoxWidth);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.textBoxX);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
@@ -77,6 +160,7 @@
             this.Load += new System.EventHandler(this.Debugging_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -85,5 +169,13 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.TextBox textBoxWidth;
+        private System.Windows.Forms.TextBox textBoxHeight;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
--- PixelAimbot/Debugging.cs
+++ PixelAimbot/Debugging.cs
@@ -16,7 +16,13 @@ namespace PixelAimbot
     public partial class Debugging : Form
     {
         private int screenWidth = Screen.PrimaryScreen.Bounds.Width;
-        private int screenHeight = Screen.PrimaryScreen.Bounds.Height; 
+        private int screenHeight = Screen.PrimaryScreen.Bounds.Height;
+        private int x = 0;
+        private int y = 0;
+        private int width = 0;
+        private int height = 0;
+        private bool isLive = false;
+        private Thread th;
         public Debugging()
         {
             InitializeComponent();
@@ -26,7 +32,7 @@ namespace PixelAimbot
         {
             Bitmap bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
             Graphics g = Graphics.FromImage(bitmap);
-            g.CopyFromScreen(1593, 40, 296, 255, bitmap.Size);
+            g.CopyFromScreen(x, y, width, height, bitmap.Size);
             pictureBox1.Image = bitmap;
         }
 
@@ -37,29 +43,67 @@ namespace PixelAimbot
 
         private void button2_Click(object sender, EventArgs e)
         {
-            byte[] buffer = {};
-            Thread th = new Thread(() => cap(buffer));
-            th.Start();
+
+            if (!isLive)
+            {
+                byte[] buffer = { };
+                th = new Thread(() => cap(buffer));
+                th.Start();
+                button2.Text = "Recording...";
+                isLive = true;
+            } else
+            {
+                button2.Text = "Live";
+                isLive = false;
+                th.Abort();
+            }
 
         }
 
         private void cap(byte[] buffer)
         {
-
-
             // throw new NotImplementedException();
             Bitmap bitmap = new Bitmap(screenWidth, screenHeight);
+            var _screenDrawer = new DrawScreen();
             while (true)
             {
                 try
                 {
-                    
+                    _screenDrawer.Draw(x, y, width * -1, height * -1);
+
                     Graphics g = Graphics.FromImage(bitmap);
-                    g.CopyFromScreen(1500, 0, 0, 0, bitmap.Size);
+                    g.CopyFromScreen(x + width, y + height, width, height, bitmap.Size);
                     pictureBox1.Image = bitmap;
                     Thread.Sleep(10);
                 } catch { }
             }
+        }
+
+        private void textBoxHeight_TextChanged(object sender, EventArgs e)
+        {
+            height = int.Parse(textBoxHeight.Text) * -1;
+            pictureBox1.Height = height * -1;
+        }
+
+        private void textBoxWidth_TextChanged(object sender, EventArgs e)
+        {
+            
+            width = int.Parse(textBoxWidth.Text) * -1;
+            pictureBox1.Width = width * -1;
+        }
+
+        private void textBoxY_TextChanged(object sender, EventArgs e)
+        {
+            y = int.Parse(textBoxY.Text);
+            
+
+        }
+
+        private void textBoxX_TextChanged(object sender, EventArgs e)
+        {
+            x = int.Parse(textBoxX.Text);
+            
+
         }
     }
 }
