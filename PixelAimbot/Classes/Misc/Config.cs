7d9b9ee9604c868bdcb8d6dbd1ae0482da4016a4
ShiiikK <102829233+ShiiikK@users.noreply.github.com>
2022-04-17 15:25:39 +0200

    versionsänderung

--- PixelAimbot/Classes/Misc/Config.cs
+++ PixelAimbot/Classes/Misc/Config.cs
@@ -15,7 +15,7 @@ namespace PixelAimbot.Classes.Misc
     
     public class Config
     {
-        public static string version { get; set; } = "2.2.3r";
+        public static string version { get; set; } = "2.2.4r";
         public string username { get; set; } = "";
         public string password { get; set; } = "";
 
