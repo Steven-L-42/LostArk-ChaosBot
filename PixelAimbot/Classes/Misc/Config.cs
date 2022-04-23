af146aa255708e272111f5e90899eb264e670211
inZaneX <54487336+inZaneX@users.noreply.github.com>
2022-04-23 22:54:49 +0200

    Set config vers

--- PixelAimbot/Classes/Misc/Config.cs
+++ PixelAimbot/Classes/Misc/Config.cs
@@ -15,7 +15,7 @@ namespace PixelAimbot.Classes.Misc
     
     public class Config
     {
-        public static string version { get; set; } = "2.3.5r";
+        public static string version { get; set; } = "2.3.6r";
         public string username { get; set; } = "";
         public string password { get; set; } = "";
         public bool earlyaccess { get; set; } = false;
