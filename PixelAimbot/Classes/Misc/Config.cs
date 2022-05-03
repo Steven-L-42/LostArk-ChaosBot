774e07e0525f3042ddca78cfdb140a2ef4b486f4
inZaneX <54487336+inZaneX@users.noreply.github.com>
2022-05-03 06:26:33 +0200

    release

--- PixelAimbot/Classes/Misc/Config.cs
+++ PixelAimbot/Classes/Misc/Config.cs
@@ -11,7 +11,7 @@ namespace PixelAimbot.Classes.Misc
     
     public class Config
     {
-        public static string version { get; set; } = "2.6.5r";
+        public static string version { get; set; } = "2.6.6r";
         public string username { get; set; } = "";
         public string password { get; set; } = "";
         public string discorduser { get; set; } = "";
