5c771dad95474ea856a848afa3875c5379390c4d
ShiiikK <102829233+ShiiikK@users.noreply.github.com>
2022-05-16 23:19:24 +0200

    program.cs

--- PixelAimbot/Program.cs
+++ PixelAimbot/Program.cs
@@ -17,7 +17,7 @@ namespace PixelAimbot
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ChaosBot());
+            Application.Run(new frmLogin());
             
         }
 
